2ac0d22f2e38fa22638b92847d794d371affdcf1
Omar <omar.assed@gmail.com>
2022-07-01 15:29:47 -0600

    commit tree 1db3fa7f676edfbf356b2b14b5ec521092696231

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+       true
+    else 
+      false
+    end
   end
 
   def question?
-
+    self.end_with?("?")
+     
   end
 
-  def exclamation?
 
+  def exclamation?
+    self.end_with?("!")
+    
   end
 
   def count_sentences
-
+    self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
   end
 end
